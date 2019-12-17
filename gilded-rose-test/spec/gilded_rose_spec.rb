211217b2b9b4d7f3cf3b7fdb63cb43f054033617
SevenSecrets <freddieshughes@googlemail.com>
2019-12-17 16:39:26 +0000

    getting simplecov working and increasing test coverage

--- gilded-rose-test/spec/gilded_rose_spec.rb
+++ gilded-rose-test/spec/gilded_rose_spec.rb
@@ -32,7 +32,9 @@ describe GildedRose do
       GildedRose.new(items).update_quality
       expect(items[0].quality).to eq 1
     end
+  end
 
+  describe "#aged_brie_update" do
     it "increases the quality of Aged Brie" do
       items = [Item.new("Aged Brie", 1, 1)]
       GildedRose.new(items).update_quality
@@ -43,6 +45,26 @@ describe GildedRose do
       items = [Item.new("Aged Brie", 1, 50)]
       GildedRose.new(items).update_quality
       expect(items[0].quality).to eq 50
+    end
+  end
+
+  describe "#backstage_passes_update" do
+    it "increases backstage pass quality by 1 when over 10 days left" do
+      items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 12, 0)]
+      GildedRose.new(items).update_quality
+      expect(items[0].quality).to eq 1
+    end
+
+    it "increases backstage pass quality by 2 when under 10 days left" do
+      items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 8, 0)]
+      GildedRose.new(items).update_quality
+      expect(items[0].quality).to eq 2
+    end
+
+    it "increases backstage pass quality by 3 when under 5 days left" do
+      items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 4, 0)]
+      GildedRose.new(items).update_quality
+      expect(items[0].quality).to eq 3
     end
 
     it "reduces backstage pass value to 0 after concert" do
