8cb9ffffef3730a39001a9df22b0c4e59f1d5c08
SevenSecrets <freddieshughes@googlemail.com>
2019-12-18 16:44:40 +0000

    tests for giving the game its parameters

--- tic-tac-toe-test/lib/game.rb
+++ tic-tac-toe-test/lib/game.rb
@@ -1,6 +1,11 @@
+require_relative 'player'
+require_relative 'field'
+require_relative 'board'
+
 class Game
-  def initialize(player1 = Player.new('x'), player2 = Player.new('o'))
+  def initialize(player1 = Player.new('x'), player2 = Player.new('o'), board = Board.new)
     @player1 = player1
     @player2 = player2
+    @board = board
   end
 end
--- tic-tac-toe-test/spec/game_spec.rb
+++ tic-tac-toe-test/spec/game_spec.rb
@@ -4,11 +4,16 @@ describe Game do
   before(:each) do
     @player1 = double('player', :fill => "x")
     @player2 = double('player', :fill => "o")
+    @board = double('board')
   end
 
   describe 'initialisation' do
     it 'exists' do
-      expect(Game.new(@player1, @player2)).to be_instance_of(Game)
+      expect(Game.new).to be_instance_of(Game)
+    end
+
+    it 'accepts parameters it needs' do
+      expect(Game.new(@player1, @player2, @board)).to be_instance_of(Game)
     end
   end
 end
