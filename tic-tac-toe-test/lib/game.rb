0bfc39b73cfff112af7a99240d2abc7d8a604c8e
SevenSecrets <freddieshughes@googlemail.com>
2019-12-19 20:21:30 +0000

    tests and functionality for taking turns and switching player

--- tic-tac-toe-test/lib/game.rb
+++ tic-tac-toe-test/lib/game.rb
@@ -6,11 +6,30 @@ class Game
   def initialize(player1 = Player.new('x'), player2 = Player.new('o'), board = Board.new)
     @player1 = player1
     @player2 = player2
+    @player_list = [@player1, @player2]
     @board = board
     @board.populate_board
+    @current_player = @player_list[0]
   end
 
   def print_gamestate
     @board.print_board
+  end
+
+  def turn(x_position, y_position)
+    field = @board.full_board[x_position][y_position]
+    @current_player.move(field)
+    next_player
+    print_gamestate
+  end
+
+  private
+
+  def next_player
+    if @current_player === @player_list[0]
+      @current_player = @player_list[1]
+    else
+      current_player = @player_list[0]
+    end
   end
 end
--- tic-tac-toe-test/spec/game_spec.rb
+++ tic-tac-toe-test/spec/game_spec.rb
@@ -1,20 +1,13 @@
 require 'game'
 
 describe Game do
-  before(:each) do
-    @player1 = double('player', :fill => "x")
-    @player2 = double('player', :fill => "o")
-    @field = double('field', :fill_in => "")
-    @board = double('board', :populate_board => [[@field, @field, @field],[@field, @field, @field],[@field, @field, @field]])
-  end
-
   describe 'initialisation' do
     it 'exists' do
       expect(Game.new).to be_instance_of(Game)
     end
 
     it 'accepts parameters it needs' do
-      expect(Game.new(@player1, @player2, @board)).to be_instance_of(Game)
+      expect(Game.new(Player.new('x'), Player.new('y'), Board.new)).to be_instance_of(Game)
     end
   end
 
@@ -22,9 +15,31 @@ describe Game do
     before(:each) do
       @game = Game.new
     end
+
     it "prints the gamestate when it's empty" do
       expect(@game.print_gamestate).to eq "[][][]\n[][][]\n[][][]\n"
     end
 
+    it "prints the gamestate when moves have been made" do
+      @game.turn(0, 0)
+      expect(@game.print_gamestate).to eq "[x][][]\n[][][]\n[][][]\n"
+    end
+  end
+
+  describe '#turn' do
+    before(:each) do
+      @game = Game.new
+    end
+
+    it 'changes the gamestate' do
+      first_gamestate = @game.turn(1, 1)
+      @game.turn(0, 0)
+      expect(@game.print_gamestate).not_to eq first_gamestate
+    end
+
+    it 'changes player each time' do
+      @game.turn(0, 0)
+      expect(@game.turn(0, 1)).to eq "[x][o][]\n[][][]\n[][][]\n"
+    end
   end
 end
