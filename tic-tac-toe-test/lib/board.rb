5bc7da002e9f0c32547a2098f18a7277095864cb
SevenSecrets <freddieshughes@googlemail.com>
2019-12-19 15:48:52 +0000

    creating tests for Board class #populate_board

--- tic-tac-toe-test/lib/board.rb
+++ tic-tac-toe-test/lib/board.rb
@@ -1,3 +1,20 @@
 class Board
 
-end 
+  attr_reader :full_board
+
+  def initialize
+    @full_board = []
+  end
+
+  def populate_board
+    3.times do
+      row = [Field.new, Field.new, Field.new]
+      @full_board << row
+    end
+    @full_board
+  end
+
+  def print_board
+    
+  end
+end
---
+++ tic-tac-toe-test/spec/board_spec.rb
@@ -0,0 +1,24 @@
+require 'board'
+
+describe Board do
+  describe 'initialisation' do
+    it 'exists' do
+      expect(Board.new).to be_instance_of Board
+    end
+  end
+
+  describe '#populate_board' do
+    it 'creates a full board' do
+      board = Board.new
+      expect(board.populate_board).to be_instance_of Array
+    end
+  end
+
+  describe '#print_board' do
+    it 'prints a full board' do
+      board = Board.new.populate_board
+      expect(board.print_board).to eq 
+    end
+  end
+
+end
