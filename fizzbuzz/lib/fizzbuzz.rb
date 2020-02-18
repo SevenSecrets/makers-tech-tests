1d27d804578d570c2d384358cc92efff297d3875
SevenSecrets <freddieshughes@googlemail.com>
2020-02-18 10:15:43 +0000

    prints fizz on 3

---
+++ fizzbuzz/lib/fizzbuzz.rb
@@ -0,0 +1,12 @@
+class FizzBuzz
+
+  def initialize
+
+  end
+
+  def play(num)
+    if(num == 3)
+      return 'fizz'
+    end
+  end
+end
--- fizzbuzz/spec/fizzbuzz_spec.rb
+++ fizzbuzz/spec/fizzbuzz_spec.rb
@@ -2,10 +2,10 @@ require 'fizzbuzz'
 
 describe 'fizzbuzz tests' do
   before(:each) do
-    fizzbuzz = FizzBuzz.new
+    @fizzbuzz = FizzBuzz.new
   end
 
   it 'prints fizz on 3' do
-    expect(fizzbuzz.play(3)).to equal 'fizz'
+    expect(@fizzbuzz.play(3)).to eq 'fizz'
   end
 end
