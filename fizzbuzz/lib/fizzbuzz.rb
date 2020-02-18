c13edea5c2a84fdd80f789c56fd30eb8ba5b4ccc
SevenSecrets <freddieshughes@googlemail.com>
2020-02-18 10:48:09 +0000

    works with play function to enter numbers individually

--- fizzbuzz/lib/fizzbuzz.rb
+++ fizzbuzz/lib/fizzbuzz.rb
@@ -5,8 +5,14 @@ class FizzBuzz
   end
 
   def play(num)
-    if(num == 3)
+    if(num % 3 == 0 && num % 5 == 0)
+      return 'fizzbuzz'
+    elsif (num % 3 == 0)
       return 'fizz'
+    elsif (num % 5 == 0)
+      return 'buzz'
+    else
+      return num
     end
   end
 end
--- fizzbuzz/spec/fizzbuzz_spec.rb
+++ fizzbuzz/spec/fizzbuzz_spec.rb
@@ -1,11 +1,27 @@
 require 'fizzbuzz'
 
-describe 'fizzbuzz tests' do
+describe 'fizzbuzz tests - specific numbers' do
   before(:each) do
     @fizzbuzz = FizzBuzz.new
   end
 
   it 'prints fizz on 3' do
     expect(@fizzbuzz.play(3)).to eq 'fizz'
+  end
+
+  it 'prints buzz on 5' do
+    expect(@fizzbuzz.play(5)).to eq 'buzz'
+  end
+
+  it 'prints fizzbuzz on 15' do
+    expect(@fizzbuzz.play(15)).to eq 'fizzbuzz'
+  end
+
+  it 'prints number on non-divisible' do
+    expect(@fizzbuzz.play(2)).to eq 2
+  end
+
+  it 'prints fizz on 6' do
+    expect(@fizzbuzz.play(6)).to eq 'fizz'
   end
 end
